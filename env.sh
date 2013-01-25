4e619df598eaa16cc8aa7654da02f220f62e50d5
Fabien Campagne <fac2003@campagnelab.org>
2013-01-25 23:38:21 +0000

    Switch to ensembl.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/campagnelab/plugins/trunk@33565 60cd2ccc-2b05-0410-9566-da273a940bff

--- env.sh
+++ env.sh
@@ -3,8 +3,11 @@
 
 # environment file to test genome download and index creation.
 export SGE_O_WORKDIR=${JOB_DIR}
+
 export ORGANISM="homo_sapiens"
 export GENOME_REFERENCE_ID="NCBI36.54"
+#export ORGANISM="Caenorhabditis_elegans"
+#export GENOME_REFERENCE_ID="WBcel215.69"
 # Fake bash GobyWeb scripts. They don't need to contain anything because we have already set the relevant variables.
 touch constants.sh
 
