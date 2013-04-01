e7bbd91cfc633484df30dab2fb69bd72fcca9501
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-01 17:01:22 -0400

    Improve comments in the registration script.

--- bin/registerFileSet.sh
+++ bin/registerFileSet.sh
@@ -6,20 +6,18 @@
 # Sample options for a remote registration:
 # --fileset-area gobyweb@spanky.med.cornell.edu:/home/gobyweb/TRIAL-FILES/GOBYWEB_FILES/A
 # --plugins-dir /Users/mas2182/Lab/Projects/Plugins/plugins2-with-tasks/test-data/root-for-rnaselect
-# --fileset-id COMPACT_READS
-# --owner Manuele.FromClusterGateway
-# --mode remote
+# --source-dir /Users/mas2182/Lab/Projects/Plugins/plugins2-with-tasks/test-data/...
+# --mode local
 # --action register
-# READS_FILE:/Users/mas2182/Lab/Projects/Plugins/plugins2-with-tasks/test-data/root-for-rnaselect/instances/fileSets/READS_1/AOUGEKP-Sample_MAN1.compact-reads
-
+# COMPACT_READS: *.compact-reads
 
 # Sample options for a local registration:
 # --fileset-area test-results
 # --plugins-dir /Users/mas2182/Lab/Projects/Plugins/plugins2-with-tasks/test-data/root-for-rnaselect
-# --fileset-id COMPACT_READS
 # --owner Manuele.FromClusterGateway
+# --source-dir /Users/mas2182/Lab/Projects/Plugins/plugins2-with-tasks/test-data/...
 # --mode local
 # --action register
-# READS_FILE:/Users/mas2182/Lab/Projects/Plugins/plugins2-with-tasks/test-data/root-for-rnaselect/instances/fileSets/READS_1/AOUGEKP-Sample_MAN1.compact-reads
+# COMPACT_READS: *.compact-reads
 
 java -cp ../lib/plugins-distro.jar:cluster_gateway.properties:$CLASSPATH org.campagnelab.gobyweb.clustergateway.registration.FileSetRegistration --action register "$@"
