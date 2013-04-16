6924c870178560ac9228e4498b337e84c9a3ab23
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-16 16:33:01 -0400

    RNA script.sh prints out tags of the registered TSV instances.

--- test-data/root-for-rnaselect/plugins/tasks/RNASELECT_TASK/script.sh
+++ test-data/root-for-rnaselect/plugins/tasks/RNASELECT_TASK/script.sh
@@ -8,18 +8,13 @@ READ_FILES_LIST=""
 function plugin_task {
 
      ${FILESET_COMMAND} --has-fileset INPUT_READS
-     #${FILESET_COMMAND} --has-fileset INPUT_READS.*
-     #${FILESET_COMMAND} --has-fileset INPUT_READS.READS_FILE
-     if [ $? == 0 ]; then
+     if [ $? != 0 ]; then
        echo Input compact reads are not available
        return 0
      fi
 
-     READ_FILES_LIST=`${FILESET_COMMAND} --fetch INPUT_READS.READS_FILE`
-     #READ_FILES_LIST=`${FILESET_COMMAND} --fetch INPUT_READS.*`
-     #READ_FILES_LIST=`${FILESET_COMMAND} --fetch INPUT_READ`
-
-     if [ $? == 0 ]; then
+     READ_FILES_LIST=`${FILESET_COMMAND} --fetch INPUT_READS`
+     if [ $? != 0 ]; then
         echo Failed to fecth compact reads
         echo ${READ_FILES_LIST}
         return 0
@@ -29,15 +24,12 @@ function plugin_task {
      java -cp rnaselect-1.0.0-tool.jar:$CLASSPATH org.campagnelab.rnaselect.App2 --output out.tsv ${READ_FILES_LIST}
 
      #push back the generated tsv
-     ${FILESET_COMMAND} --push STATS: **/*.tsv
-     #${FILESET_COMMAND} --push STATS: out.tsv
-     #${FILESET_COMMAND} --push **/*.tsv
-     #${FILESET_COMMAND} --push out.tsv
-
-      if [ $? == 0 ]; then
+     REGISTERED_TAGS=`${FILESET_COMMAND} --push STATS: *.tsv`
+     if [ $? != 0 ]; then
         echo Failed to push back the output TSV file
         return 0
      fi
+     echo "RNA-select registered the following FileSet instances: ${REGISTERED_TAGS}"
 }
 
 
