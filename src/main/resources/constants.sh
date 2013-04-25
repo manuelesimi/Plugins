6c486156026a963209d0df53db9c27c0754fd0e7
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-25 15:58:14 -0400

    Add fileset-area-cache to FILESET_COMMAND in constants.sh. Update fileset.jar.

--- src/main/resources/constants.sh
+++ src/main/resources/constants.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 JOB_DIR=%%JOB_DIR%%
+TMPDIR=`pwd` #TODO: temporary, it has to be replaced with the real TMPDIR when analyses and aligners will be submitted with the Cluster Gateway
 TAG=%%TAG%%
 ARTIFACT_REPOSITORY_DIR=%%ARTIFACT_REPOSITORY_DIR%%
 INITIAL_STATE=task
 RESOURCES_GOBYWEB_SERVER_SIDE_FILESETS_JAR="filesets.jar"
 RESOURCES_GOBYWEB_SERVER_SIDE_DEPENDENCIES_JAR="serverside-dependencies.jar"
-FILESET_COMMAND="java -cp ${RESOURCES_GOBYWEB_SERVER_SIDE_FILESETS_JAR}:${RESOURCES_GOBYWEB_SERVER_SIDE_DEPENDENCIES_JAR} org.campagnelab.gobyweb.filesets.JobInterface --job-tag %%TAG%%"
+FILESET_COMMAND="java -cp ${RESOURCES_GOBYWEB_SERVER_SIDE_FILESETS_JAR}:${RESOURCES_GOBYWEB_SERVER_SIDE_DEPENDENCIES_JAR} org.campagnelab.gobyweb.filesets.JobInterface --fileset-area-cache ${TMPDIR} --job-tag %%TAG%%"
 GOBY_DIR=${JOB_DIR}
 
 set -x
