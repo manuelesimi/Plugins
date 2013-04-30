3eed6aef9f0d3cc1ae5eb412bcde6fa811df1799
Manuele Simi <manuele.simi@campagnelab.org>
2013-04-30 17:11:41 -0400

    Add template for artifacts.

---
+++ templates/scripts/artifact-template.sh
@@ -0,0 +1,22 @@
+# Plugins SDK - Artifact installation template
+
+
+#This function is expected to an install the artifact in the given installation path
+#Parameters:
+# $1: the ID of the artifact to install as specified in the resource configuration
+# $2: the target installation path
+function plugin_install_artifact {
+    #sample parameters reading
+    ID=$1
+    INSTALLATION_PATH=$2
+
+     #function implementation
+
+}
+
+#attribute logic
+function get_attribute_values {
+
+    #function implementation
+
+}
