351e71c7c3df99165f9828f24baab7d92e816c79
Manuele Simi <manuele.simi@campagnelab.org>
2013-05-07 10:51:34 -0400

    Add templates folder to the distribution package. Also, distribute a template of env.sh.

---
+++ templates/artifact-config/env.sh
@@ -0,0 +1,33 @@
+# JOB_DIR is defined by the caller.
+JOB_DIR=%%JOB_DIR%%
+
+if [ -e ${JOB_DIR}/constants.sh ]; then
+    . ${JOB_DIR}/constants.sh
+else
+    touch ${JOB_DIR}/constants.sh
+fi
+
+if [ -e ${JOB_DIR}/auto-options.sh ]; then
+    . ${JOB_DIR}/auto-options.sh
+else
+  touch ${JOB_DIR}/auto-options.sh
+fi
+
+
+# environment file to test genome download and index creation.
+export SGE_O_WORKDIR=${JOB_DIR}
+
+export ORGANISM="homo_sapiens"
+export GENOME_REFERENCE_ID="NCBI36.54"
+#export ORGANISM="Caenorhabditis_elegans"
+#export GENOME_REFERENCE_ID="WBcel215.69"
+# Fake bash GobyWeb scripts. They don't need to contain anything because we have already set the relevant variables.
+touch constants.sh
+# Put wget in the path, on my mac:
+PATH=${PATH}:/sw/bin/
+
+# This function must exist in an environment script:
+
+function plugin_install_artifact {
+    return 0
+}
