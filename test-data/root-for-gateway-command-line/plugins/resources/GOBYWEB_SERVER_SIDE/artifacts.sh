a2c931058433eebdfdc43e303ec8f456c42cf85f
Fabien Campagne <fac2003@campagnelab.org>
2013-05-27 14:06:50 -0400

    Test steplogs with root-for-gateway command line.

--- test-data/root-for-gateway-command-line/plugins/resources/GOBYWEB_SERVER_SIDE/artifacts.sh
+++ test-data/root-for-gateway-command-line/plugins/resources/GOBYWEB_SERVER_SIDE/artifacts.sh
@@ -3,11 +3,12 @@ function install_plugin_artifacts {
     echo "Installing plugin resources"
     if [ -e ${SGE_O_WORKDIR}/artifacts-install-requests.pb ]; then
         mkdir -p ${ARTIFACT_REPOSITORY_DIR}
+        mkdir -p ${JOB_DIR}/steplogs
         REPO_MANAGER_OPTIONS="--repo-dir-quota  1000000000"
         RUN_ARTIFACT_MANAGER="java -Dlog4j.configuration=file:${GOBY_DIR}/log4j.properties \
                  -Djava.io.tmpdir=${TMPDIR} \
                  -cp ${GOBY_DIR}/serverside-dependencies.jar:${GOBY_DIR}/artifact-manager.jar \
-                 org.campagnelab.gobyweb.artifacts.ArtifactManager "
+                 org.campagnelab.gobyweb.artifacts.ArtifactManager --log-dir ${JOB_DIR}/steplogs "
 
         ${RUN_ARTIFACT_MANAGER} \
                 --ssh-requests  ${SGE_O_WORKDIR}/artifacts-install-requests.pb \
@@ -23,7 +24,7 @@ function install_plugin_artifacts {
         ${RUN_ARTIFACT_MANAGER} \
             --repository ${ARTIFACT_REPOSITORY_DIR} ${REPO_MANAGER_OPTIONS} \
             --bash-exports --ssh-requests  ${SGE_O_WORKDIR}/artifacts-install-requests.pb \
-            --output exports.sh
+            --output exports.sh --log-dir ${JOB_DIR}/steplogs
 
         cat exports.sh
         . exports.sh
