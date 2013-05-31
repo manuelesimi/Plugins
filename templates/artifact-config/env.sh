97e23504db9bec91b523a1aad075223e834b93b6
Manuele Simi <manuele.simi@campagnelab.org>
2013-05-31 15:28:13 -0400

    Add a clean template for environment script.

--- templates/artifact-config/env.sh
+++ templates/artifact-config/env.sh
@@ -1,9 +1,10 @@
+# This script connects the artifact manager to the GobyWeb environment. It will be source
+# before any artifact installation function is executed.
 # JOB_DIR is defined by the caller.
-
 if [ -e ${JOB_DIR}/constants.sh ]; then
     . ${JOB_DIR}/constants.sh
 else
-    touch ${JOB_DIR}/constants.sh
+  touch ${JOB_DIR}/constants.sh
 fi
 
 if [ -e ${JOB_DIR}/auto-options.sh ]; then
@@ -12,18 +13,8 @@ else
   touch ${JOB_DIR}/auto-options.sh
 fi
 
-
 # environment file to test genome download and index creation.
 export SGE_O_WORKDIR=${JOB_DIR}
-
-export ORGANISM="homo_sapiens"
-export GENOME_REFERENCE_ID="NCBI36.54"
-#export ORGANISM="Caenorhabditis_elegans"
-#export GENOME_REFERENCE_ID="WBcel215.69"
-# Fake bash GobyWeb scripts. They don't need to contain anything because we have already set the relevant variables.
-touch constants.sh
-# Put wget in the path, on my mac:
-PATH=${PATH}:/sw/bin/
 
 # This function must exist in an environment script:
 
