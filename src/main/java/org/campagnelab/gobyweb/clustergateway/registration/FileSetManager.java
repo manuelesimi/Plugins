ac6b598f16c44bca46c88bf1c922621bc71ec19a
Manuele Simi <manuele.simi@campagnelab.org>
2014-03-13 16:23:32 -0400

    Print out to the user the stacktrace when FilesetManager fails.

--- src/main/java/org/campagnelab/gobyweb/clustergateway/registration/FileSetManager.java
+++ src/main/java/org/campagnelab/gobyweb/clustergateway/registration/FileSetManager.java
@@ -53,7 +53,7 @@ public class FileSetManager {
             process(args);
             System.exit(0);
         } catch (Exception e) {
-            logger.error("FileSetManager failed to process the request.");
+            logger.error("FileSetManager failed to process the request.",e);
             System.exit(1);
         }
     }
