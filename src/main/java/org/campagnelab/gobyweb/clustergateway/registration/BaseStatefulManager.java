fe68bfa995fcdc2dc2a0c295994668cd07d3754f
Manuele Simi <manuele.simi@campagnelab.org>
2014-05-08 12:44:17 -0400

    Add interface and implementation of common methods for StatefulManagers.

--- src/main/java/org/campagnelab/gobyweb/clustergateway/registration/BaseStatefulManager.java
+++ src/main/java/org/campagnelab/gobyweb/clustergateway/registration/BaseStatefulManager.java
@@ -1,6 +1,9 @@
 package org.campagnelab.gobyweb.clustergateway.registration;
 
+import org.campagnelab.gobyweb.filesets.FileSetAPI;
 import org.campagnelab.gobyweb.filesets.configuration.ConfigurationList;
+import org.campagnelab.gobyweb.filesets.preview.RegistrationPreviewDetails;
+import org.campagnelab.gobyweb.filesets.registration.InputEntry;
 import org.campagnelab.gobyweb.io.AreaFactory;
 import org.campagnelab.gobyweb.io.FileSetArea;
 import org.campagnelab.gobyweb.plugins.PluginRegistry;
@@ -8,13 +11,14 @@ import org.campagnelab.gobyweb.plugins.xml.filesets.FileSetConfig;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Base version of stateful FileSetManager.
  *
  * @author manuele
  */
-public class BaseStatefulManager implements Serializable {
+public abstract class BaseStatefulManager implements Serializable,StatefulFileSetManager {
 
     protected  FileSetArea storageArea;
 
@@ -32,9 +36,31 @@ public class BaseStatefulManager implements Serializable {
      * @param pluginRegistry
      * @throws Exception
      */
-    public void setPluginDefinitions(PluginRegistry pluginRegistry) throws Exception {
+    public void setPluginDefinitions(PluginRegistry pluginRegistry) {
         this.pluginRegistry = pluginRegistry;
         configurationList = PluginsToConfigurations.convertAsList(pluginRegistry.filterConfigs(FileSetConfig.class));
+    }
+    /**
+     * Previews the registration of the given paths.
+     * @param paths paths to preview
+     * @param fileSetID an optional fileset ID to use for the preview.
+     * @return
+     * @throws Exception
+     */
+    public RegistrationPreviewDetails previewRegistration(
+            String[] paths, String ... fileSetID) throws Exception {
+        //convert plugins configuration to configurations that can be consumed by FileSetAPI
+        FileSetAPI fileset = FileSetAPI.getReadWriteAPI(storageArea, configurationList);
+        List<InputEntry> inputEntries;
+        if (fileSetID != null && fileSetID.length > 0) {
+            String[] entries = new String[paths.length + 1];
+            entries[0] = fileSetID[0] + ":";
+            System.arraycopy(paths,0,entries,1,paths.length);
+            inputEntries = FileSetManager.parseInputEntries(entries);
+        }  else {
+            inputEntries = FileSetManager.parseInputEntries(paths);
+        }
+        return fileset.registerPreview(inputEntries,fileSetID);
     }
 
 }
--- src/main/java/org/campagnelab/gobyweb/clustergateway/registration/StatefulFileSetManager.java
+++ src/main/java/org/campagnelab/gobyweb/clustergateway/registration/StatefulFileSetManager.java
@@ -1,36 +1,24 @@
 package org.campagnelab.gobyweb.clustergateway.registration;
 
-import org.campagnelab.gobyweb.filesets.FileSetAPI;
-import org.campagnelab.gobyweb.filesets.configuration.ConfigurationList;
 import org.campagnelab.gobyweb.filesets.preview.RegistrationPreviewDetails;
 import org.campagnelab.gobyweb.filesets.protos.MetadataFileReader;
-import org.campagnelab.gobyweb.filesets.registration.InputEntry;
-import org.campagnelab.gobyweb.filesets.rpc.FileSetClient;
-import org.campagnelab.gobyweb.io.AreaFactory;
-import org.campagnelab.gobyweb.io.FileSetArea;
 import org.campagnelab.gobyweb.plugins.PluginRegistry;
-import org.campagnelab.gobyweb.plugins.Plugins;
-import org.campagnelab.gobyweb.plugins.xml.filesets.FileSetConfig;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 /**
- * A stateful version of the FileSetManager.
- *
- * @author manuele
+ * Created by mas2182 on 5/8/14.
  */
-public class StatefulFileSetManager extends BaseStatefulManager {
+public interface StatefulFileSetManager {
 
-    private static final long serialVersionUID = 1526246795621236347L;
-
-    public StatefulFileSetManager(String filesetAreaReference, String owner) throws IOException {
-       super(filesetAreaReference,owner);
-    }
+    /**
+     * Sets the plugin definitions to use for the manager's operation.
+     * @param pluginRegistry
+     * @throws Exception
+     */
+    public void setPluginDefinitions(PluginRegistry pluginRegistry);
 
     /**
      * Previews the registration of the given paths.
@@ -39,21 +27,7 @@ public class StatefulFileSetManager extends BaseStatefulManager {
      * @return
      * @throws Exception
      */
-    public RegistrationPreviewDetails previewRegistration(
-                String[] paths, String ... fileSetID) throws Exception {
-        //convert plugins configuration to configurations that can be consumed by FileSetAPI
-        FileSetAPI fileset = FileSetAPI.getReadWriteAPI(storageArea, configurationList);
-        List<InputEntry> inputEntries;
-        if (fileSetID != null && fileSetID.length > 0) {
-            String[] entries = new String[paths.length + 1];
-            entries[0] = fileSetID[0] + ":";
-            System.arraycopy(paths,0,entries,1,paths.length);
-            inputEntries = FileSetManager.parseInputEntries(entries);
-        }  else {
-            inputEntries = FileSetManager.parseInputEntries(paths);
-        }
-        return fileset.registerPreview(inputEntries,fileSetID);
-    }
+    public RegistrationPreviewDetails previewRegistration(String[] paths, String ... fileSetID) throws Exception;
 
     /**
      * Registers a single instance fileset instance using the given type.
@@ -66,14 +40,15 @@ public class StatefulFileSetManager extends BaseStatefulManager {
      * @return the list of tags of the newly registered filesets.
      * @throws Exception
      */
-    public List<String> register(String fileSetID,
-            String[] paths, final Map<String, String> attributes,
-            final List<String> sharedWith, List<String> errors, String tag) throws Exception {
-        String[] entries = new String[paths.length + 1];
-        entries[0] = fileSetID + ":";
-        System.arraycopy(paths,0,entries,1,paths.length);
-        FileSetAPI fileset = FileSetAPI.getReadWriteAPI(storageArea, configurationList);
-        List<InputEntry> inputEntries = FileSetManager.parseInputEntries(entries);
-        return fileset.register(inputEntries,attributes,sharedWith,errors,tag);
-    }
+    public List<String> register(String fileSetID, String[] paths, final Map<String, String> attributes,
+                                 final List<String> sharedWith, List<String> errors, String tag) throws Exception;
+
+    /**
+     * Fetches the instance metadata.
+     * @param tag
+     * @param errors
+     * @return
+     * @throws IOException
+     */
+    public MetadataFileReader fetchMetadata(String tag, List<String> errors) throws IOException;
 }
