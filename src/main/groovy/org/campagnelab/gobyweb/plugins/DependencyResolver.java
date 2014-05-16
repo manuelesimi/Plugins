853a03f3ab5b6f48136ecfbd15b70af84aeba803
Fabien Campagne <fac2003@campagnelab.org>
2014-05-16 15:14:30 -0400

    Introduce the StatefulDependencyResolver.

--- src/main/groovy/org/campagnelab/gobyweb/plugins/DependencyResolver.java
+++ src/main/groovy/org/campagnelab/gobyweb/plugins/DependencyResolver.java
@@ -8,21 +8,25 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- *
+ * Resolve dependencies against the global singleton repository. See StatefulDependencyResolver to resolve against
+ * an arbitrary PluginRepository.
  */
 public class DependencyResolver {
 
     private static PluginRegistry pluginConfigs = PluginRegistry.getRegistry();
+    private static StatefulDependencyResolver resolver= new StatefulDependencyResolver(pluginConfigs);
+
 
     /**
-      * Return the resource with largest version number with the specified identifier.
-      *
-      * @param resourceId
-      * @return Most recent resource (by version number) with id and version>v
-      */
-     public static ResourceConfig resolveResource(String resourceId) {
-         return resolveResource(resourceId, null, null, null);
-     }
+     * Return the resource with largest version number with the specified identifier.
+     *
+     * @param resourceId
+     * @return Most recent resource (by version number) with id and version>v
+     */
+    public static ResourceConfig resolveResource(String resourceId) {
+        return resolver.resolveResource(resourceId);
+    }
+
     /**
      * Return the resource with largest version number, such that the resource has the identifier and at least the specified
      * version number.
@@ -32,7 +36,7 @@ public class DependencyResolver {
      * @return Most recent resource (by version number) with id and version>v
      */
     public static ResourceConfig resolveResource(String resourceId, String versionAtLeast) {
-        return resolveResource(resourceId, versionAtLeast, null, null);
+        return resolver.resolveResource(resourceId, versionAtLeast, null, null);
     }
 
     /**
@@ -45,9 +49,12 @@ public class DependencyResolver {
      * @return Most recent resource (by version number) with id and version>v
      */
     public static ResourceConfig resolveResource(String resourceId, String versionAtLeast, String versionExactly) {
-        return resolveResource(resourceId, versionAtLeast, versionExactly, null);
+        return resolver.resolveResource(resourceId, versionAtLeast, versionExactly, null);
     }
 
+    public static FileSetConfig resolveFileSetFromMimeType(String mimeType) {
+        return resolver.resolveFileSetFromMimeType(mimeType);
+    }
     /**
      * Return the resource with largest version number, such that the resource has the identifier and at least the specified
      * version number.
@@ -59,32 +66,8 @@ public class DependencyResolver {
      * @return Most recent resource (by version number) with id and version>v
      */
     public static ResourceConfig resolveResource(String resourceId, String versionAtLeast, String versionExactly, String versionAtMost) {
-        List<ResourceConfig> resourceList = new ArrayList<ResourceConfig>();
-        synchronized (pluginConfigs) {
-            for (ResourceConfig resource : pluginConfigs.filterConfigs(ResourceConfig.class)) {
-                if (!resource.getId().equalsIgnoreCase(resourceId)) continue;
-
-                if (versionExactly != null && !resource.exactlyVersion(versionExactly)) {
-                    continue;
-                }
-                if (versionAtLeast != null && !resource.atLeastVersion(versionAtLeast)) {
-                    continue;
-                }
-                if (versionAtMost != null && !resource.atMostVersion(versionAtMost)) {
-                    continue;
-                }
-
-                resourceList.add(resource);
-
-            }
-        }
-        if (resourceList.size() > 0) {
-            Collections.sort(resourceList);
-            return resourceList.get(0);
-        } else
-            return null;
+        return resolver.resolveResource(resourceId, versionAtLeast, versionExactly, versionAtMost);
     }
-
     /**
      * Return the fileset with largest version number, such that the resource has the identifier and at least the specified
      * version number.
@@ -96,59 +79,6 @@ public class DependencyResolver {
      * @return Most recent resource (by version number) with id and version>v
      */
     public static FileSetConfig resolveFileSet(String fileSetId, String versionAtLeast, String versionExactly, String... versionAtMost) {
-        List<FileSetConfig> fileSetList = new ArrayList<FileSetConfig>();
-        boolean checkAtMost = false;
-        if (versionAtMost != null && versionAtMost.length > 0 && versionAtMost[0] != null) {
-            checkAtMost = true;
-        }
-        synchronized (pluginConfigs) {
-            for (FileSetConfig fileSet : pluginConfigs.filterConfigs(FileSetConfig.class)) {
-                if (versionExactly != null) { //check exactly
-                    if (fileSet.getId().equalsIgnoreCase(fileSetId) &&
-                            fileSet.exactlyVersion(versionExactly))
-                        fileSetList.add(fileSet);
-                } else if (versionAtLeast != null) {
-                    if (fileSet.getId().equalsIgnoreCase(fileSetId) &&
-                            fileSet.atLeastVersion(versionAtLeast)) {
-                        if (checkAtMost) { //check atLeast and atMost
-                            if (fileSet.atMostVersion(versionAtMost[0])) {
-                                fileSetList.add(fileSet);
-                            }
-                        } else  //accept because there is not atMost to check
-                            fileSetList.add(fileSet);
-                    }
-                } else if (checkAtMost && (fileSet.getId().equalsIgnoreCase(fileSetId))) { //check only atMost
-                    if (fileSet.atMostVersion(versionAtMost[0])) {
-                        fileSetList.add(fileSet);
-                    }
-                }
-            }
-        }
-        if (fileSetList.size() > 0) {
-            Collections.sort(fileSetList);
-            return fileSetList.get(0);
-        } else
-            return null;
-
+        return resolver.resolveFileSet(fileSetId, versionAtLeast, versionExactly, versionAtMost);
     }
-
-    public static FileSetConfig resolveFileSetFromMimeType(String mimeType) {
-        //look for matching fileset configurations
-        List<FileSetConfig> matchingFileSets = new ArrayList<FileSetConfig>();
-        synchronized (pluginConfigs) {
-            for (FileSetConfig fileSet : pluginConfigs.filterConfigs(FileSetConfig.class)) {
-                if (fileSet.getMimeType().equalsIgnoreCase(mimeType)) {
-                   matchingFileSets.add(fileSet);
-                }
-            }
-        }
-        //pick up the matching fileset with largest version number
-        if (matchingFileSets.size() > 0) {
-            Collections.sort(matchingFileSets);
-            return matchingFileSets.get(0);
-        } else
-            return null;
-
-    }
-
 }
---
+++ src/main/groovy/org/campagnelab/gobyweb/plugins/StatefulDependencyResolver.java
@@ -0,0 +1,158 @@
+package org.campagnelab.gobyweb.plugins;
+
+import org.campagnelab.gobyweb.plugins.xml.filesets.FileSetConfig;
+import org.campagnelab.gobyweb.plugins.xml.resources.ResourceConfig;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ ** Resolve dependencies against a GobyWeb PluginRepository.
+ */
+public class StatefulDependencyResolver  {
+
+    private PluginRegistry pluginConfigs = PluginRegistry.getRegistry();
+
+    public StatefulDependencyResolver(PluginRegistry pluginConfigs) {
+        this.pluginConfigs = pluginConfigs;
+    }
+
+    /**
+      * Return the resource with largest version number with the specified identifier.
+      *
+      * @param resourceId
+      * @return Most recent resource (by version number) with id and version>v
+      */
+     public  ResourceConfig resolveResource(String resourceId) {
+         return resolveResource(resourceId, null, null, null);
+     }
+    /**
+     * Return the resource with largest version number, such that the resource has the identifier and at least the specified
+     * version number.
+     *
+     * @param resourceId
+     * @param versionAtLeast required version
+     * @return Most recent resource (by version number) with id and version>v
+     */
+    public  ResourceConfig resolveResource(String resourceId, String versionAtLeast) {
+        return resolveResource(resourceId, versionAtLeast, null, null);
+    }
+
+    /**
+     * Return the resource with largest version number, such that the resource has the identifier and exactly the specified
+     * version number.
+     *
+     * @param resourceId
+     * @param versionAtLeast required version
+     * @param versionExactly required exact this version
+     * @return Most recent resource (by version number) with id and version>v
+     */
+    public  ResourceConfig resolveResource(String resourceId, String versionAtLeast, String versionExactly) {
+        return resolveResource(resourceId, versionAtLeast, versionExactly, null);
+    }
+
+    /**
+     * Return the resource with largest version number, such that the resource has the identifier and at least the specified
+     * version number.
+     *
+     * @param resourceId
+     * @param versionAtLeast required version
+     * @param versionExactly required exact this version
+     * @param versionAtMost  max required version
+     * @return Most recent resource (by version number) with id and version>v
+     */
+    public ResourceConfig resolveResource(String resourceId, String versionAtLeast, String versionExactly, String versionAtMost) {
+        List<ResourceConfig> resourceList = new ArrayList<ResourceConfig>();
+        synchronized (pluginConfigs) {
+            for (ResourceConfig resource : pluginConfigs.filterConfigs(ResourceConfig.class)) {
+                if (!resource.getId().equalsIgnoreCase(resourceId)) continue;
+
+                if (versionExactly != null && !resource.exactlyVersion(versionExactly)) {
+                    continue;
+                }
+                if (versionAtLeast != null && !resource.atLeastVersion(versionAtLeast)) {
+                    continue;
+                }
+                if (versionAtMost != null && !resource.atMostVersion(versionAtMost)) {
+                    continue;
+                }
+
+                resourceList.add(resource);
+
+            }
+        }
+        if (resourceList.size() > 0) {
+            Collections.sort(resourceList);
+            return resourceList.get(0);
+        } else
+            return null;
+    }
+
+    /**
+     * Return the fileset with largest version number, such that the resource has the identifier and at least the specified
+     * version number.
+     *
+     * @param fileSetId
+     * @param versionAtLeast required version
+     * @param versionExactly required exact this version
+     * @param versionAtMost  max required version
+     * @return Most recent resource (by version number) with id and version>v
+     */
+    public FileSetConfig resolveFileSet(String fileSetId, String versionAtLeast, String versionExactly, String... versionAtMost) {
+        List<FileSetConfig> fileSetList = new ArrayList<FileSetConfig>();
+        boolean checkAtMost = false;
+        if (versionAtMost != null && versionAtMost.length > 0 && versionAtMost[0] != null) {
+            checkAtMost = true;
+        }
+        synchronized (pluginConfigs) {
+            for (FileSetConfig fileSet : pluginConfigs.filterConfigs(FileSetConfig.class)) {
+                if (versionExactly != null) { //check exactly
+                    if (fileSet.getId().equalsIgnoreCase(fileSetId) &&
+                            fileSet.exactlyVersion(versionExactly))
+                        fileSetList.add(fileSet);
+                } else if (versionAtLeast != null) {
+                    if (fileSet.getId().equalsIgnoreCase(fileSetId) &&
+                            fileSet.atLeastVersion(versionAtLeast)) {
+                        if (checkAtMost) { //check atLeast and atMost
+                            if (fileSet.atMostVersion(versionAtMost[0])) {
+                                fileSetList.add(fileSet);
+                            }
+                        } else  //accept because there is not atMost to check
+                            fileSetList.add(fileSet);
+                    }
+                } else if (checkAtMost && (fileSet.getId().equalsIgnoreCase(fileSetId))) { //check only atMost
+                    if (fileSet.atMostVersion(versionAtMost[0])) {
+                        fileSetList.add(fileSet);
+                    }
+                }
+            }
+        }
+        if (fileSetList.size() > 0) {
+            Collections.sort(fileSetList);
+            return fileSetList.get(0);
+        } else
+            return null;
+
+    }
+
+    public FileSetConfig resolveFileSetFromMimeType(String mimeType) {
+        //look for matching fileset configurations
+        List<FileSetConfig> matchingFileSets = new ArrayList<FileSetConfig>();
+        synchronized (pluginConfigs) {
+            for (FileSetConfig fileSet : pluginConfigs.filterConfigs(FileSetConfig.class)) {
+                if (fileSet.getMimeType().equalsIgnoreCase(mimeType)) {
+                   matchingFileSets.add(fileSet);
+                }
+            }
+        }
+        //pick up the matching fileset with largest version number
+        if (matchingFileSets.size() > 0) {
+            Collections.sort(matchingFileSets);
+            return matchingFileSets.get(0);
+        } else
+            return null;
+
+    }
+
+}
