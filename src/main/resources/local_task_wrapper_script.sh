7a834b2cb23c1496a7a8eef4af5f17b8115c6781
Fabien Campagne <fac2003@campagnelab.org>
2017-06-13 12:27:42 -0400

    Define debug function (used by artifact.sh script).
    (cherry picked from commit 6780445)

--- src/main/resources/local_task_wrapper_script.sh
+++ src/main/resources/local_task_wrapper_script.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-
- #include logging functions
-. %JOB_DIR%/message-functions.sh
-
-. %JOB_DIR%/job_common_functions.sh
+function debug {
+    echo "$*";
+}
 
 function setup_task_functions {
     # define no-op function to be overridden as needed by task script:
@@ -14,7 +12,15 @@ function setup_task_functions {
 
 }
 
+function dieUponError {
+  RETURN_STATUS=$?
+  DESCRIPTION=$1
+  if [ ! ${RETURN_STATUS} -eq 0 ]; then
+       echo "Task failed. Error description: ${DESCRIPTION}"
+       exit
+  fi
 
+}
 
 function install_resources {
      #include needed function for resources installation
@@ -26,6 +32,21 @@ function install_resources {
       fi
 }
 
+# Pushes some job metadata to the fileset area
+# param $1: space-separated list of fileset tags registered by the job
+function push_job_metadata {
+   tags="$@"
+   rm -rf ${JOB_DIR}/${TAG}.properties
+   rm -rf %FILESET_AREA%/${TAG:0:1}/${TAG}
+   echo "JOB=${TAG}" >> ${JOB_DIR}/${TAG}.properties
+   echo "OWNER=${OWNER}" >> ${JOB_DIR}/${TAG}.properties
+   echo "PLUGIN=${PLUGIN_ID}" >> ${JOB_DIR}/${stats_file}
+   echo "COMPLETED=`date +"%Y-%m-%d %T%z"`" >> ${JOB_DIR}/${TAG}.properties
+   echo "TAGS=${tags}" >> ${JOB_DIR}/${TAG}.properties
+   echo "SHAREDWITH=" >> ${JOB_DIR}/${TAG}.properties
+   REGISTERED_TAGS=`${FILESET_COMMAND} --push --fileset-tag ${TAG} JOB_METADATA: ${JOB_DIR}/${TAG}.properties`
+   echo "The following JOB_METADATA instance has been successfully registered: ${REGISTERED_TAGS}"
+}
 
 function run_task {
    ALL_REGISTERED_TAGS=""
@@ -61,17 +82,18 @@ function run_task {
 
     fi
 
-    #make sure that the dir in which reads files will be stored exists
-    mkdir -p ${FILESET_TARGET_DIR}
+     #make sure that the dir in which reads files will be stored exists
+     mkdir -p ${FILESET_TARGET_DIR}
 
-    jobStarted
     setup_task_functions
     install_resources
-    debug "Required resources installed."
+
+
     LOG_FILE="run-task-`date "+%Y-%m-%d-%H:%M:%S"`.log"
     run_task 2>&1 |tee ${LOG_FILE}
     if [ $?==0 ]; then
-      jobCompleted
+      echo "Task execution completed successfully." >>${LOG_FILE}
+
     else
-      jobFailed
+      echo "An error occurred"
     fi
