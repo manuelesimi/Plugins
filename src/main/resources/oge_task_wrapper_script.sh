2a31220d95c607f78a42aeec280a452cc06be703
Manuele Simi <manuele.simi@campagnelab.org>
2017-07-07 15:03:21 -0400

    Remove duplicate submission for tasks (probably introduced by the merging with the feature branch).

--- src/main/resources/oge_task_wrapper_script.sh
+++ src/main/resources/oge_task_wrapper_script.sh
@@ -157,7 +157,6 @@ case ${STATE} in
            HOLD_OPTION="-hold_jid ${JOBS_HOLD_LIST}"
         fi
         SUBMISSION=`qsub -N ${TAG}.submit ${HOLD_OPTION} -terse -l ${PLUGIN_NEED_PROCESS} -r y -v STATE=${INITIAL_STATE} oge_task_wrapper_script.sh`
-        SUBMISSION=`qsub -N ${TAG}.submit -terse -l ${PLUGIN_NEED_PROCESS} -r y -v STATE=task oge_task_wrapper_script.sh`
         echo ${SUBMISSION}
         ;;
 
