43fbc9e07b00ee3b314705692032478f41162f4c
Manuele Simi <manuele.simi@campagnelab.org>
2017-07-07 15:48:37 -0400

    Use GOBYWEB_SERVER_SIDE 2.7.

--- src/main/groovy/org/campagnelab/gobyweb/plugins/PluginLoaderSettings.java
+++ src/main/groovy/org/campagnelab/gobyweb/plugins/PluginLoaderSettings.java
@@ -46,7 +46,7 @@ public class PluginLoaderSettings {
      */
     public static final String[] SERVER_SIDE_TOOL = new String[] {
             "GOBYWEB_SERVER_SIDE", //ID
-            "2.6",  //versionAtLeast
+            "2.7",  //versionAtLeast
             null, //versionExactly
             null //versionAtMost
     };
