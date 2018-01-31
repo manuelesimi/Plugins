9fd57fbb0731dfe8b0e0389eb65a46c46941917d
Manuele Simi <manuele.simi@campagnelab.org>
2018-01-31 10:58:24 -0500

    Invoke print_OGE_env after the job environment has been initialized.
    (cherry picked from commit 2b048da)

--- src/main/resources/oge_job_script.sh
+++ src/main/resources/oge_job_script.sh
@@ -176,11 +176,11 @@ if [ -z "${STATE+set}" ]; then
  echo "Defined STATE=${STATE}"
 fi
 #see where we are running
-print_OGE_env
 
 case ${STATE} in
     submit)
         initializeJobEnvironment
+        print_OGE_env
         setup
         cd ${JOB_DIR}
         SUBMISSION=`qsub -N ${TAG}.submit -r y -terse -v STATE=${INITIAL_STATE} oge_job_script.sh `
@@ -192,12 +192,14 @@ case ${STATE} in
     pre_align)
         initializeJobEnvironment
         export STATE="pre_align"
+        print_OGE_env
         delegate_oge_job_script ${STATE} "$*"
         submit_align
         ;;
 
     diffexp)
         initializeJobEnvironment
+        print_OGE_env
         if [ "${SPLIT_PROCESS_COMBINE}" == "false" ]; then
            delegate_oge_job_script "diffexp_sequential"
          else
